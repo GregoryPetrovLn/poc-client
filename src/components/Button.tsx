24e402aef7d7f51d13cba91e56eb87a6023681d5
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 16:50:22 +0300

    added button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  onClick: () => void;
+  text: string;
+  className?: string;
+}
+
+const Button = ({ onClick, text, className }: Props) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`${className} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer`}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default Button;
