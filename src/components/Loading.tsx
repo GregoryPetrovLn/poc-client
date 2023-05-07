6f3daafab2ce33003126bf3956726750b00a164f
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 16:50:33 +0300

    added loader

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { BeatLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <BeatLoader color="white" loading={true} />
+    </div>
+  );
+};
+
+export default Loading;
