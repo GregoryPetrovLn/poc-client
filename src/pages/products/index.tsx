a8fde826c5b127ccbe97ec6ac38755349b4a1a00
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 18:45:16 +0300

    added routing

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,21 +1,23 @@
 import Card from "@/components/Card";
 import Table from "@/components/Table";
+import { useRouter } from "next/router";
 import process from "process";
+import { FC } from "react";
 
 interface Props {
   data: Product[] | null;
   count: number;
 }
+const tableRender = [
+  { id: "name", label: "Name" },
+  { id: "price", label: "Price" },
+  { id: "quantity", label: "Quantity" },
+];
+const Products: FC<Props> = ({ data, count }) => {
+  const router = useRouter();
 
-const Products = ({ data, count }: Props) => {
-  const tableRender = [
-    { id: "name", label: "Name" },
-    { id: "price", label: "Price" },
-    { id: "quantity", label: "Quantity" },
-  ];
-  const onRowClickHandler = (id: string) => {
-    console.log("Clicket->", id);
-  };
+  const onRowClickHandler = (id: string) => router.push(`/products/${id}`);
+
   return (
     <Card>
       <div className="text-2xl font-bold mb-4 flex justify-between items-center">
