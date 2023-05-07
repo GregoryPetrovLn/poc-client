fb0b77aa6132927f2305c539b04ed589cb6843fe
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 17:17:53 +0300

    init app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import InitAppWrapper from "@/wrappers/InitAppWrapper";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 import { Providers } from "./provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
       <InitAppWrapper>
-        <Component {...pageProps} />
+        <Toaster />
+        <div className="h-screen overflow-y-scroll bg-slate-200">
+          {/* Header */}
+          <Component {...pageProps} />
+        </div>
       </InitAppWrapper>
     </Providers>
   );
