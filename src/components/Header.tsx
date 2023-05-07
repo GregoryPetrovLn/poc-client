0e597e7dc9413652e268fcdec2eabb4a46451118
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 18:46:31 +0300

    added link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { HomeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 const Header = () => {
   return (
     <div className="shadow-md w-screen h-auto px-5 py-3 flex justify-between items-center">
-      <div className="text-xl flex items-center">
-        <HomeIcon className="w-7 h-7 mr-2" />
-        Online Market
-      </div>
+      <Link href={"/products"}>
+        <div className="text-xl flex items-center">
+          <HomeIcon className="w-7 h-7 mr-2" />
+          Online Market
+        </div>
+      </Link>
       <div className="">
         <button className="font-bold text-lg px-4 py-2 hover:bg-gray-300 rounded">
           {false ? "user.name" : "Login"}
