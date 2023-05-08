fb84c24648a1c5cf81224e1f3c5e52c174b8cd74
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-08 12:07:21 +0300

    added delete logic

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,12 +1,41 @@
 import Card from "@/components/Card";
+import { AUTH_TOKEN } from "@/service/localStorageItems";
+import { getItemFromLocalStorage } from "@/service/utils";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 import { FC } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../../store/slices/user/userSlice";
 
 interface Props {
   data: Product | null;
 }
 const Product: FC<Props> = ({ data }) => {
   const { name, description, price, quantity } = data || {};
+  const { user } = useSelector(userSelector);
+  const isAdmin = user?.role === "admin";
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const notification = toast.loading("Deleting...");
+    try {
+      await fetch(`${process.env.API_BASE_URL}/products/${data?._id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getItemFromLocalStorage(AUTH_TOKEN)}`,
+        },
+      });
+      router.push("/products");
+      toast.success("Product deleted", { id: notification });
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong", { id: notification });
+    }
+  };
+
   return (
     <Card>
       <h1 className="text-center text-3xl shadow-b-md">Product overview</h1>
@@ -27,9 +56,28 @@ const Product: FC<Props> = ({ data }) => {
           </div>
         </div>
         <div>
-          <span className="text-gray-300 hover:text-gray-400 cursor-default">
-            Edit mode only for admins
-          </span>
+          {!isAdmin && (
+            <span className="text-gray-300 hover:text-gray-400 cursor-default">
+              Edit mode only for admins
+            </span>
+          )}
+
+          {isAdmin && (
+            <div className="flex flex-col space-y-3 text-right">
+              <div
+                onClick={handleDelete}
+                className="text-red-400 hover:text-red-500 cursor-pointer flex items-center justify-end"
+              >
+                Delete
+                <TrashIcon className="h-5 w-5" />
+              </div>
+
+              <div className="text-blue-400 hover:text-blue-500 cursor-pointer flex items-center justify-end">
+                Edit
+                <PencilSquareIcon className="h-5 w-5" />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </Card>
